6aa005c3e40cca4a218951fa1f5b9e8ea9539c0d
musa mustapha <mustykolo6@gmail.com>
2025-07-19 14:58:47 +0100

    Updated GeoJSON file names in 1_file_path_config.py

--- core/1_file_path_config.py
+++ core/1_file_path_config.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 # --- Configuration: SET YOUR FILE PATHS AND COLUMN NAMES HERE ---
 ROAD_LINES_FILE = "ROAD_NETWORK_finale.geojson"
-ROAD_INTERSECTIONS_FILE = "all intersections pro.geojson"
-MAIN_BUILDINGS_FILE = "all school buildings pro.geojson"
+ROAD_INTERSECTIONS_FILE = "all_intersections_pro.geojson"
+MAIN_BUILDINGS_FILE = "all_school_buildings_pro.geojson"
 
 QGIS_NAME_COLUMN = 'Name'
 QGIS_UNIQUE_ID_COLUMN = 'id' # IMPORTANT: Set to 'id' (lowercase) to match QGIS export
